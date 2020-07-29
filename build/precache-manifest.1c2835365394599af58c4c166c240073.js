e966f3e2eb160f3e1c21700c8f31564e4c1a25ca
Vanderson Ribeiro <vanderson.ribeiro01@gmail.com>
2020-07-29 14:37:23 -0300

    set incial

---
+++ build/precache-manifest.1c2835365394599af58c4c166c240073.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f463b1ccc858ec2072f3888f29e8b2d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5ed1cb110b95a48ed95",
+    "url": "/static/css/main.30db1e61.chunk.css"
+  },
+  {
+    "revision": "74c9a5b322fbc8028a8c",
+    "url": "/static/js/2.311004dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.311004dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5ed1cb110b95a48ed95",
+    "url": "/static/js/main.716dc9c3.chunk.js"
+  },
+  {
+    "revision": "c04bc2f752e856d17039",
+    "url": "/static/js/runtime-main.3c3c2bf2.js"
+  },
+  {
+    "revision": "4b13cc33a8eb1b27425c5aafbab3573e",
+    "url": "/static/media/Gatonetiflix.4b13cc33.png"
+  }
+]);
