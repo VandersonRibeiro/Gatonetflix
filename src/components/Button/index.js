3251f3ace6c90fa00a2bf67d48a9fe29f2981729
Vanderson Ribeiro <vanderson.ribeiro01@gmail.com>
2020-08-07 01:27:55 -0300

    vai que vai

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
+
 const Button = styled.button`
     color: var(--white);
     border: 1px solid var(--white);
     background: var(--black)
-    box-sizing: border-box;
-    cursor: pointer;
+    box-sizing:border-box;
+    cursor pointer;
     padding: 16px 24px;
     font-style: normal;
     font-weight: bold;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,7 +30,9 @@ function CadastroCategoria() {
 
   useEffect(() => {
     console.log('Fora bozooo');
-    const URL= 'http://localhost:8080/categorias';
+    const URL= window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080' 
+    : 'https://gatonetflix.herokuapp.com/categorias';
     fetch(URL)
       .then(async (respostaDoServer) => {
         const resposta = await respostaDoServer.json();
